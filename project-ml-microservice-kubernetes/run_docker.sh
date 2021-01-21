342110153a199c1c602d5b23247c493f1f37340c
brianx0215 <brianxu0215@gmail.com>
2021-01-21 06:37:51 +0000

    Fill docker configurations

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag bxbbx/udapredict:1.0 .
 
 # Step 2: 
 # List docker images
+docker images --all
 
 # Step 3: 
 # Run flask app
+docker run -it -p 8000:80 bxbbx/udapredict:1.0
